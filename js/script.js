f9c5da300fcf9249604152b3dacaeac8c2ae9a1c
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-08-30 14:00:16 +0200

    Add second counter script.js

--- js/script.js
+++ js/script.js
@@ -22,17 +22,54 @@ var Counter = React.createClass({
             React.createElement('div', {className: 'container'},
             
             React.createElement('div', {onClick: this.increment},
-            React.createElement('button', {}, 'Add ' + this.state.counter)
-        ),
-        React.createElement('div', {onClick: this.decrement},
-            React.createElement('button', {}, 'Substract' + this.state.counter)    
-        )
-    )
+                React.createElement('button', {}, 'Add ' + this.state.counter)
+             ),
+            React.createElement('div', {onClick: this.decrement},
+                React.createElement('button', {}, 'Substract ' + this.state.counter)    
+                )
             )
+        )
          
     }
-
-
 });
+
+var secondCounter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 2
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 2
+        });
+    },
+
+    render: function() {
+        return ( 
+            React.createElement('div', {className: 'container'},
+            
+            React.createElement('div', {onClick: this.increment},
+                React.createElement('button', {}, 'Add ' + this.state.counter)
+             ),
+            React.createElement('div', {onClick: this.decrement},
+                React.createElement('button', {}, 'Substract ' + this.state.counter)    
+                )
+            )
+        )
+         
+    }
+});
+
 var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
+
+var element2 = React.createElement(secondCounter);
+ReactDOM.render(element2, document.getElementById('app2'));
